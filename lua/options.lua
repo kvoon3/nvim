efde6eefffb68bdd33cd01c1fb8fd95f705979b6
kvoon3 <kvoon@duck.com>
2026-06-15 19:19:20 +0800

    feat(options): set cursor style to block in normal and line in terminal mode

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,8 @@
 vim.opt.number = true -- show absolute number
 vim.opt.relativenumber = false -- add numbers to each line on the left side
 vim.opt.cursorline = false -- highlight cursor line underneath the cursor horizontally
+vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25'
+-- Cursor style: block for normal/visual/command, vertical line for insert, horizontal for replace/operator, line for terminal mode
 vim.opt.splitbelow = true -- open new vertical split bottom
 vim.opt.splitright = true -- open new horizontal splits right
 
@@ -31,4 +33,3 @@ vim.opt.smartcase = true -- override ignorecase when pattern contains uppercase 
 vim.opt.laststatus = 2 -- always show statusline
 vim.opt.statusline = "%f %m%r%h%w %= [%l,%c] [%p%%] [%L lines]"
 -- Format: filename modified-flag readonly help-flag preview-flag | line,column | percentage | total-lines
-
